d0f33b918fba596ca206c363c55ba5bddc4fbb39
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-14 13:47:58 -0400

    create employee class with name, email, phone, and salary

--- employee_review_test.rb
+++ employee_review_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 
 #Note: This line is going to fail first.
 require './department.rb'
+require './employee'
 
 class EmployeeReviewTest < Minitest::Test
 
@@ -13,6 +14,14 @@ class EmployeeReviewTest < Minitest::Test
   def test_create_department_name
     department = Department.new("Development")
     assert "Development", department.name
+  end
+
+  def test_create_employee
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+    assert "Dutch Matrix", employee.name
+    assert "Commando@example.com", employee.email
+    assert "919-877-1276", employee.phone
+    assert 90000, employee.salary
   end
 
 end
