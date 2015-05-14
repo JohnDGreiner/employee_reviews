861af9cd367e1d8241e2c0faf5f234d13ad37e22
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-14 13:52:58 -0400

    split up into some more tests

--- employee_review_test.rb
+++ employee_review_test.rb
@@ -17,10 +17,26 @@ class EmployeeReviewTest < Minitest::Test
   end
 
   def test_create_employee
+    Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+  end
+
+  def test_get_employee_name
     employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
     assert "Dutch Matrix", employee.name
+  end
+
+  def test_get_employee_email
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
     assert "Commando@example.com", employee.email
+  end
+
+  def test_get_employee_phone
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
     assert "919-877-1276", employee.phone
+  end
+
+  def test_get_employee_salary
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
     assert 90000, employee.salary
   end
 
