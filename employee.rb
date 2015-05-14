70554c1056043152c10a7064b6369412a45f48bc
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-14 14:02:07 -0400

    employee has a department

--- employee.rb
+++ employee.rb
@@ -1,11 +1,12 @@
 class Employee
   attr_reader :name, :email, :phone, :salary
 
-  def initialize(name, email, phone, salary)
+  def initialize(name, email, phone, salary, department)
     @name = name
     @email = email
     @phone = phone
     @salary = salary
+    @department = department
   end
 
 end
--- employee_review_test.rb
+++ employee_review_test.rb
@@ -17,27 +17,37 @@ class EmployeeReviewTest < Minitest::Test
   end
 
   def test_create_employee
-    Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+    department = Department.new("Development")
+    Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000, department)
   end
 
   def test_get_employee_name
-    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+    department = Department.new("Development")
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000, department)
     assert "Dutch Matrix", employee.name
   end
 
   def test_get_employee_email
-    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+    department = Department.new("Development")
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000, department)
     assert "Commando@example.com", employee.email
   end
 
   def test_get_employee_phone
-    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+    department = Department.new("Development")
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000, department)
     assert "919-877-1276", employee.phone
   end
 
   def test_get_employee_salary
-    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000)
+    department = Department.new("Development")
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000, department)
     assert 90000, employee.salary
+  end
+
+  def test_employee_has_department
+    department = Department.new("Development")
+    employee = Employee.new("Dutch Matrix","Commando@example.com","919-877-1276", 90000, department)
   end
 
 end
